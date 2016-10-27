e7db668b8ddaee28363d9399e98e470eb57a3aa3
cuyu <icyarm@qq.com>
2016-10-27 11:03:41 +0800

    Add example.

---
+++ list_all_files_recursively.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Curtis Yu
+@contact: cuyu@splunk.com
+@since: 10/27/16
+"""
+import fnmatch
+import os
+
+
+def list_files(root_path, filter='*'):
+    matches = []
+    for root, dirnames, filenames in os.walk(root_path):
+        for filename in fnmatch.filter(filenames, filter):
+            matches.append(os.path.join(root, filename))
+
+    return matches
+
+
+if __name__ == '__main__':
+    print list_files('/tmp')
+    print list_files('/tmp', '*.log')
